758802bb252eb058a0457208a6bc8275ecc0c015
aurelwidyanti <aurelwyt@gmail.com>
2024-09-07 17:31:32 +0700

    refactor: Add CourseController to API routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticationController;
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\LectureController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -13,4 +14,5 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
     Route::resource('/lecturers', LectureController::class);
+    Route::resource('/courses', CourseController::class);
 });
